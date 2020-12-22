e21ba3e574164b760708a43db5c30a7bcb117709
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2020-12-22 15:25:20 +0530

    [vidyadhar hudge]Add:Test case for indian state census analyser csv file incorrect then return exception

--- IndianStateCensusAnalyserTest/UnitTest1.cs
+++ IndianStateCensusAnalyserTest/UnitTest1.cs
@@ -10,6 +10,7 @@ namespace IndianStateCensusAnalyserTest
     {
         static string indianStateCensusHeaders = "State,Population,AreaInSqKm,DensityPerSqKm";
         static string indianStateCensusFilePath = @"\Users\hp\source\repos\IndianStateDemo\CensusAnalyserTests\CSVFile\IndianStateCensusData.csv";
+        static string wrongIndianStateCensusFilePath = @"\Users\hp\source\repos\IndianStateDemo\CensusAnalyserTests\CSVFile\WrongIndianStateCensusData.csv";
 
         CensusAnalyser censusAnalyser;
         Dictionary<string, CensusDTO> totalRecord;
@@ -32,6 +33,16 @@ namespace IndianStateCensusAnalyserTest
         {
             totalRecord = censusAnalyser.LoadCensusData(Country.INDIA, indianStateCensusFilePath, indianStateCensusHeaders);
             Assert.AreEqual(29, totalRecord.Count);
+        }
+
+        /// <summary>
+        /// Test Case 1.2 Given the indian census data file when incorrect then return File not found exception.
+        /// </summary>
+        [Test]
+        public void GivenIndianCensusDataFile_WhenIncorrect_ThenShouldReturnFileNotFoundException()
+        {
+            var censusException =Assert.Throws<CensusAnalyserException>(()=> censusAnalyser.LoadCensusData(Country.INDIA, wrongIndianStateCensusFilePath, indianStateCensusHeaders));
+            Assert.AreEqual(CensusAnalyserException.ExceptionType.FILE_NOT_FOUND, censusException.exceptionType);
         }
     }
 }
