34b49206e831072616e27963a84bf504809ecd1d
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2020-12-22 14:31:26 +0530

    [vidyadhar hudge]Add:Welcome Message

---
+++ Indian StatesCensusAnalyser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Indian_StatesCensusAnalyser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to indian state census analyser program");
+        }
+    }
+}
